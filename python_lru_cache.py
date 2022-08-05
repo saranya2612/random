ba4e855c61fabe105cb8bafa7cbc4efa935d1d86
saranya2612 <saranya261290@gmail.com>
2022-08-05 17:16:40 +0530

    python LRUcache implementation

---
+++ python_lru_cache.py
@@ -0,0 +1,91 @@
+st="""
+Write a query find number of
+employees
+whose
+DOB is between
+02 / 05 / 1970
+to
+31 / 12 / 1975 and are
+grouped
+according
+to
+gender.
+
+select *
+from table_name where
+
+table_name.dob
+
+LRU
+Cache
+Implement
+the
+LRUCache
+
+
+class:
+
+
+LRUCache(int
+capacity) Initialize
+the
+LRU
+cache
+with positive size capacity.
+int
+get(int
+key) Return
+the
+value
+of
+the
+key if the
+key
+exists, otherwise
+return -1.
+void
+put(int
+key, int
+value) Update
+the
+value
+of
+the
+key if the
+key
+exists.Otherwise, add
+the
+key - value
+pair
+to
+the
+cache.If
+the
+number
+of
+keys
+exceeds
+the
+capacity
+from this operation, evict
+
+the
+least
+recently
+used
+key.
+The
+functions
+get and put
+must
+each
+run in O(1)
+average
+time
+complexity.
+"""
+
+p=st.split('\n')
+
+s=' '.join(p)
+print(s)
